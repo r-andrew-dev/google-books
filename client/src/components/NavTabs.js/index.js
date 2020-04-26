9119d8b38e9e1ee93d86b54728471de5b756adf3
Rachael Andrew <r.andrew.dev@gmail.com>
2020-04-26 00:12:25 -0500

    removed extra unused declarations

--- client/src/components/NavTabs.js/index.js
+++ client/src/components/NavTabs.js/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Navbar, Nav } from 'react-bootstrap/';
-import { PromiseProvider } from 'mongoose';
 
 
 function NavTabs(props) {
--- client/src/components/pages/Search.js
+++ client/src/components/pages/Search.js
@@ -14,7 +14,6 @@ class Search extends Component {
         description: '',
         image: '',
         link: '',
-        message,
     }
 
     handleInputChange = event => {
